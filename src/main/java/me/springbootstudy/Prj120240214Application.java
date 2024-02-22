4357e3d98f9cab61f5ae759f0470467993864b93
ShinMinGi <140944460+ShinMinGi@users.noreply.github.com>
2024-02-22 15:52:47 +0900

    first
    prj1 - first 24.02.22

---
+++ src/main/java/me/springbootstudy/Prj120240214Application.java
@@ -0,0 +1,13 @@
+package me.springbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj120240214Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj120240214Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/springbootstudy/Prj120240214ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj120240214ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
